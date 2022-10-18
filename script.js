cbe1b679775048738d60271ed6fb71045d5910a1
RobertRish <robert.rish1225@gmail.com>
2022-10-17 22:34:29 -0400

    add buzz feature

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 let answer = parseInt(prompt("How long should this list of numberzzz bee?"));
 
 for (let i = 0; i <= answer; i++) {
-    console.log(i);
+    // "%" is called modulus operator which returns the remainder of a division problem.  
+    // the statement below is saying, "if the remainder of i divided by 3 is 0..."
     if (i % 3 === 0) {
         console.log("Fizz");
+    } else if (i % 5 === 0) {
+        console.log("Buzz");
     } else {
         console.log(i);
     }
