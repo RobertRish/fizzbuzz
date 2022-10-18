4528fa2061b4907b3b4babb6d6e64684df48bf91
RobertRish <robert.rish1225@gmail.com>
2022-10-17 22:45:24 -0400

    complete problem

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 let answer = parseInt(prompt("How long should this list of numberzzz bee?"));
 
-for (let i = 0; i <= answer; i++) {
+for (let i = 1; i <= answer; i++) {
     // "%" is called modulus operator which returns the remainder of a division problem.  
     // the statement below is saying, "if the remainder of i divided by 3 is 0..."
-    if (i % 3 === 0) {
-        console.log("Fizz");
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz")
     } else if (i % 5 === 0) {
         console.log("Buzz");
+    } else if (i % 3 === 0) {
+        console.log("Fizz")
     } else {
         console.log(i);
     }
-}
+};
