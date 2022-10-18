66bfac278855ebc30066822d5a39f81c0c8fe8d4
RobertRish <robert.rish1225@gmail.com>
2022-10-17 20:49:30 -0400

    add feature to print fizz instead multiples of 3

--- script.js
+++ script.js
@@ -2,4 +2,9 @@ let answer = parseInt(prompt("How long should this list of numberzzz bee?"));
 
 for (let i = 0; i <= answer; i++) {
     console.log(i);
+    if (i % 3 === 0) {
+        console.log("Fizz");
+    } else {
+        console.log(i);
+    }
 }
